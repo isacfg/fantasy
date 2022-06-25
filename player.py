6242dc84f7123da8d4742a7ed55ee6b71f556fb0
isacfg <isaac.lima.105@ufrn.edu.br>
2022-06-25 15:18:08 -0300

    basics

--- player.py
+++ player.py
@@ -7,23 +7,24 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.test_player = pygame.image.load(os.path.join('assets', 'testPlayer.png'))
-        self.test_player = pygame.transform.scale(self.test_player, (50, 50))
+        self.test_player = pygame.transform.scale(self.test_player, (50, 68.75))
         self.rect = self.test_player.get_rect()
+        self.speed = 5
  
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
-            self.rect.y -= 10
+            self.rect.y -= self.speed
         
         if keys[pygame.K_s]:
-            self.rect.y += 10
+            self.rect.y += self.speed
         
         if keys[pygame.K_a]:
-            self.rect.x -= 10
+            self.rect.x -= self.speed
         
         if keys[pygame.K_d]:
-            self.rect.x += 10
+            self.rect.x += self.speed
 
     def update(self):
         self.get_input()
