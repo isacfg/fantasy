1f37618bed6163c1f8cdd72e4104379e7db4f55e
isacfg <isaac.lima.105@ufrn.edu.br>
2022-07-03 20:57:16 -0300

    removed particles

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ settings = Settings()
 menu = Menu(0)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, create_jump_particles):
+    def __init__(self, pos, surface):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0 # current sprite frame
@@ -19,11 +19,11 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         
         # dust particles
-        self.import_dust_run_particles()
-        self.dust_frame_index = 0 # current sprite frame
-        self.dust_animation_speed = 0.15 # animation speed
-        self.display_surface = surface
-        self.create_jump_particles = create_jump_particles
+        # self.import_dust_run_particles()
+        # self.dust_frame_index = 0 # current sprite frame
+        # self.dust_animation_speed = 0.15 # animation speed
+        # self.display_surface = surface
+        # self.create_jump_particles = create_jump_particles
 
         # player movement
         self.direction = pygame.math.Vector2(0, 0)
@@ -61,25 +61,25 @@ class Player(pygame.sprite.Sprite):
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path, True) # True to scale to seetings player width and height, useful por que particulas nao precisam escalar
 
-    def import_dust_run_particles(self): # run particles
-        self.dust_run_particles = import_folder('./assets/player/dust_particles/run')
+    # def import_dust_run_particles(self): # run particles
+    #     self.dust_run_particles = import_folder('./assets/player/dust_particles/run')
 
-    def run_dust_animation(self):
-        if self.status == 'run' and self.on_ground:
-            self.dust_frame_index += self.dust_animation_speed
-            if self.dust_frame_index >= len(self.dust_run_particles):
-                self.dust_frame_index = 0
+    # def run_dust_animation(self):
+    #     if self.status == 'run' and self.on_ground:
+    #         self.dust_frame_index += self.dust_animation_speed
+    #         if self.dust_frame_index >= len(self.dust_run_particles):
+    #             self.dust_frame_index = 0
 
-            dust_particle = self.dust_run_particles[int(self.dust_frame_index)]
+    #         dust_particle = self.dust_run_particles[int(self.dust_frame_index)]
 
-            if self.facing_right:
-                pos = self.rect.bottomleft - pygame.math.Vector2(6, 10)
-                self.display_surface.blit(dust_particle, pos)
+    #         if self.facing_right:
+    #             pos = self.rect.bottomleft - pygame.math.Vector2(6, 10)
+    #             self.display_surface.blit(dust_particle, pos)
 
-            if self.facing_right == False:
-                pos = self.rect.bottomright - pygame.math.Vector2(6, 10)
-                flipped_dust_particle = pygame.transform.flip(dust_particle, True, False)
-                self.display_surface.blit(flipped_dust_particle, pos)
+    #         if self.facing_right == False:
+    #             pos = self.rect.bottomright - pygame.math.Vector2(6, 10)
+    #             flipped_dust_particle = pygame.transform.flip(dust_particle, True, False)
+    #             self.display_surface.blit(flipped_dust_particle, pos)
 
     def animate(self):
         animation = self.animations[self.status]
@@ -143,7 +143,7 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
-            self.create_jump_particles(self.rect.midbottom)
+            # self.create_jump_particles(self.rect.midbottom)
 
     def get_status(self): 
         if self.direction.y < 0:
@@ -174,5 +174,5 @@ class Player(pygame.sprite.Sprite):
         self.get_input()
         self.get_status()
         self.animate()
-        self.run_dust_animation()
+        # self.run_dust_animation()
         # print(self.dust_run_particles)
